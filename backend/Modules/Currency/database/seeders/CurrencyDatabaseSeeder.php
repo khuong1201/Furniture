09922a31148a351703664d485cf76f2ef6d1791c
khuong1201 <khuong6315@gmail.com>
2025-12-06 00:59:56 +0700

    update

--- backend/Modules/Currency/database/seeders/CurrencyDatabaseSeeder.php
+++ backend/Modules/Currency/database/seeders/CurrencyDatabaseSeeder.php
@@ -12,7 +12,7 @@ class CurrencyDatabaseSeeder extends Seeder
         // 1 VND = 1 VND
         Currency::firstOrCreate(['code' => 'VND'], [
             'name' => 'Vietnam Dong',
-            'symbol' => '₫',
+            'symbol' => 'VND',
             'exchange_rate' => 1.00000000,
             'is_default' => true,
             'is_active' => true
@@ -21,7 +21,7 @@ class CurrencyDatabaseSeeder extends Seeder
         // 1 VND = 0.000039 USD (Giả định 1 USD = 25,600 VND)
         Currency::firstOrCreate(['code' => 'USD'], [
             'name' => 'US Dollar',
-            'symbol' => '$',
+            'symbol' => 'USD',
             'exchange_rate' => 0.00003906, 
             'is_default' => false,
             'is_active' => true
